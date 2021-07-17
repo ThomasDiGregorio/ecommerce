a8d6a5f826d693e89d775a19534a27cc63f6f674
ThomasDiGregorio <digregoriothomas@yahoo.com>
2021-07-17 16:53:02 -0400

    fixed broken styles component

--- src/components/details.js
+++ src/components/details.js
@@ -4,7 +4,7 @@ class Details extends Component {
   render() {
     const { className, title, links } = this.props;
     return (
-      <div className={`${className} "details"`}>
+      <div className={`${className} details`}>
         <div className="details__title">{title}</div>
         <div className="details__links">
           {links.map((link, index) => {
