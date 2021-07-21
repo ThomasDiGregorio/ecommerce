21fc74a4b49f1a360fcdca83135bac8cff46718f
ThomasDiGregorio <digregoriothomas@yahoo.com>
2021-07-21 19:44:58 -0400

    built headernavbar reducer

--- src/components/details.js
+++ src/components/details.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 class Details extends Component {
   renderChildren = () => {
+    console.log(this.props);
     const { links, info } = this.props;
     let children = [];
     if (links) {
