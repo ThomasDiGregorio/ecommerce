c5bc28597e1e49053ebfebad368d0fa2c9fbb67b
ThomasDiGregorio <digregoriothomas@yahoo.com>
2021-10-17 15:10:02 -0400

    built order review products component

--- src/components/headernavbar/header.js
+++ src/components/headernavbar/header.js
@@ -14,7 +14,7 @@ class Header extends Component {
               <a
                 className="header__link"
                 key={index}
-                onClick={() => history.pushState(link.path)}
+                onClick={() => history.push(link.path)}
               >
                 {link.title}
               </a>
